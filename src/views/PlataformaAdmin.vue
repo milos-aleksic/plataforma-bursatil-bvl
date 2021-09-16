4956c347b1fbad9ca4098574284a5add981f4f00
Milos Aleksic <milos@aleksic.es>
2021-09-15 21:33:11 -0500

    Example of default export object

--- src/views/PlataformaAdmin.vue
+++ src/views/PlataformaAdmin.vue
@@ -13,8 +13,11 @@ import listEmisoresBVL from '@/components/listEmisoresBVL.vue'
 import TickerListAdmin from '@/components/TickerListAdmin.vue'
 import TickerCreate from '@/components/TickerCreate.vue'
 
-export default {
+const Obj = {
   name: 'PlataformaAdmin',
   components: {  listEmisoresBVL, TickerListAdmin, TickerCreate }
 }
+
+export default Obj
+
 </script>
